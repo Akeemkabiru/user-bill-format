aac292362efdceb429e493faa240c4e07557a62d
Kabby <kabby.dev@outlook.com>
2024-03-11 02:09:32 +0000

    initial

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nameOne string = "Akeem"
+	var nameTwo = "Kabiru"
+	fmt.Println(nameOne, nameTwo)
+	nameOne = "Ezekiel"
+	fmt.Println(nameOne)
+	nameThree := "sunday"
+	fmt.Println(nameThree)
+	var ageOne int = 20
+	fmt.Println(ageOne)
+}
+
+//the shorthand for declaring variable cannot be used outside a function but var can be used to declare variable outside a function
