b1d9dda3de78384cb180c5519f5a01b28e75f840
Kabby <kabby.dev@outlook.com>
2024-03-13 11:52:09 +0000

    loops

--- server.go
+++ server.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
 	studentsNames := []string{"d", "a", "c"}
 	studentsScore := []int{2, 3, 1, 4, 5}
-	fmt.Println(append(studentsNames, "d"))
-	sort.Strings(studentsNames)
+	//these loops do not mutate the slices cos we still have same values in the slice
+	for i := 0; i < len(studentsScore); i++ {
+		fmt.Println(studentsScore[i] + 2)
+	}
+	for index, value := range studentsNames {
+		fmt.Println(index, value)
+	}
+	fmt.Println(studentsScore)
+	//fmt.Println(append(studentsNames, "d"))
+	//sort.Strings(studentsNames)
+	//
+	//sort.Ints(studentsScore)
+	//fmt.Println(sort.SearchInts(studentsScore, 1))
+	//fmt.Println(studentsNames, studentsScore)
 
-	sort.Ints(studentsScore)
-	fmt.Println(sort.SearchInts(studentsScore, 1))
-	fmt.Println(studentsNames, studentsScore)
 }
