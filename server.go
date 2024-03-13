b64496e5979c804146523b7b18ab40e2f6b08c8b
Kabby <kabby.dev@outlook.com>
2024-03-13 19:35:11 +0000

    using function

--- server.go
+++ server.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+func areaOfCircle(r float64) string {
+	area := fmt.Sprintf("0.1%f", math.Pi*r*r)
+	return area
+}
+
+func goodBye(n string) {
+	fmt.Printf("Hello %v \n", n)
+}
+func welcome(n []string, f func(string)) {
+	for _, value := range n {
+		fmt.Println(value)
+	}
+}
+
 func main() {
-	studentsNames := []string{"d", "a", "c"}
-	studentsScore := []int{2, 3, 1, 4, 5}
-	//these loops do not mutate the slices cos we still have same values in the slice
-	for i := 0; i < len(studentsScore); i++ {
-		fmt.Println(studentsScore[i] + 2)
-	}
-	for index, value := range studentsNames {
-		fmt.Println(index, value)
-	}
-	fmt.Println(studentsScore)
-	//fmt.Println(append(studentsNames, "d"))
-	//sort.Strings(studentsNames)
-	//
-	//sort.Ints(studentsScore)
-	//fmt.Println(sort.SearchInts(studentsScore, 1))
-	//fmt.Println(studentsNames, studentsScore)
+	goodBye("Kabby")
+	welcome([]string{"akeem", "kabiru"}, goodBye)
+	areaOne := areaOfCircle(2)
+	areaTwo := areaOfCircle(3)
+	fmt.Println(areaTwo, areaOne)
 
 }
