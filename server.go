ddd5383f1bd8684a615925e8f2fc492ad038391e
Kabby <kabby.dev@outlook.com>
2024-03-13 20:57:33 +0000

    double return

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 func areaOfCircle(r float64) string {
@@ -19,11 +20,22 @@ func welcome(n []string, f func(string)) {
 	}
 }
 
+func userName(n string) (string, string) {
+	strSlice := strings.Split(n, " ")
+	fmt.Println(strSlice)
+	var initials []string
+	for _, value := range strSlice {
+		initials = append(initials, value[:1])
+	}
+
+	return initials[0], initials[1]
+}
 func main() {
 	goodBye("Kabby")
 	welcome([]string{"akeem", "kabiru"}, goodBye)
 	areaOne := areaOfCircle(2)
 	areaTwo := areaOfCircle(3)
 	fmt.Println(areaTwo, areaOne)
+	fmt.Println(userName("Akeem Kabiru"))
 
 }
