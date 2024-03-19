838224fa64324946dfb6507d992f09d99e6f5db7
Kabby <kabby.dev@outlook.com>
2024-03-19 22:15:26 +0000

    bill

--- bill.go
+++ bill.go
@@ -1,86 +1,54 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-type user struct {
-	name     string
-	email    string
-	password string
-	products map[string]float64
-}
-
-func newUser(name string, email string, password string, products map[string]float64) user {
-	firstUser := user{
-		name:     name,
-		email:    email,
-		password: password,
-		products: products,
-	}
-	return firstUser
-}
-
-func (firstUser user) userName() string {
-	firstLetter := firstUser.name[:2]
-	return strings.ToUpper(firstLetter)
-}
+import "fmt"
 
 type bill struct {
-	name string
-	bill map[string]float64
-	tip  float64
+	name  string
+	bills map[string]float64
+	tip   float64
 }
 
+// creating new bill for user
 func newBill(name string) bill {
 	b := bill{
-		name: name,
-		bill: map[string]float64{},
-		tip:  0,
+		name:  name,
+		bills: map[string]float64{"macbook": 20, "phone": 30, "workspace": 100},
+		tip:   0,
 	}
 	return b
 }
 
-// receiver function
-func (b bill) format() string {
-	var fs string
-	var total float64 = 0
-	for key, value := range b.bill {
-		fs += fmt.Sprintf("%v...%-25v \n", key, value)
-		total += value
+// Receiver function to format the bill
+func (b bill) formatBills() string {
+	var totalBills float64
+	var formattedBills string
+	for key, value := range b.bills {
+		formattedBills += fmt.Sprintf("%-12v...%v\n", key, value)
+		//total bills
+		totalBills = totalBills + value
 	}
-	fs += fmt.Sprintf("total...%-25v \n", total)
-	return fs
+	return fmt.Sprintf("%-10v\ntotal bills...%v", formattedBills, totalBills)
 }
 
-func (b bill) updateBill(product string, price float64) map[string]float64 {
-	b.bill = map[string]float64{product: price}
-	return b.bill
-}
-
-func (b bill) updateTips(newTip float64) float64 {
-	b.tip = newTip
+// Receiver function that update bill and tip
+func (b bill) updateTip(billValue float64) float64 {
+	b.tip = billValue
 	return b.tip
 }
 
-func (b bill) updateName(n string) string {
-	b.name = n
-	return n
+// Receiver function that update the bill
+func (b bill) addBill(product string, price float64) map[string]float64 {
+	b.bills = map[string]float64{product: price}
+	return b.bills
 }
 
-// slice
-var scores []int = []int{1, 2, 3, 4}
-
-// map
-var users map[string]string = map[string]string{"Akeem": "Man"}
-
-//receiver function that update the bill and change the tip
-
-// Pointer and Memory
-func memo() {
-	age := 20
-	m := &age
-	fmt.Println(m)  //ampersand is used to get location of a value in memory
-	fmt.Println(*m) //asteriks is used to find value in a particular memory location
+// main
+func main() {
+	bill := newBill("Akeem")
+	fmt.Println(bill)
+	formatted := newBill("kabiru").formatBills()
+	fmt.Println(formatted)
+	newTip := newBill("akeem").updateTip(30)
+	fmt.Println(newTip)
+	fmt.Println(newBill("Dorcas"))
 }
